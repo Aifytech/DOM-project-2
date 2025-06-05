40b7603e30238f6b46bfc56248c0c530232a06c9
aifytech24@gmail.com <aifytech24@gmail.com>
2025-06-05 08:39:33 +0100

    DOM project 2

---
+++ script.js
@@ -0,0 +1,19 @@
+function getRandomHexColor() {
+  const hexChars = '01234567890ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++){
+    let randomIndex = Math.floor(Math.random() * 16);
+    color += hexChars[randomIndex];
+  }
+  return color;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const changeColorBtn = document.getElementById('change-color-btn');
+  changeColorBtn.addEventListener('click', () => {
+  document.getElementById('color-box').style.backgroundColor = getRandomHexColor();
+  }); 
+});
+
+
+
